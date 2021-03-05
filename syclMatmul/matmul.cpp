daf3478721822957ff8d1a626023d1ef3304ef2b
u65695 <u65695@login-2.aidevcloud>
2021-03-05 00:46:32 -0800

    update

--- syclMatmul/matmul.cpp
+++ syclMatmul/matmul.cpp
@@ -1,8 +1,37 @@
 #include <CL/sycl.hpp>
+#include "oneapi/mkl.hpp"
 
 int main(int argc,  char** argv){
+    cl::sycl::queue matmul_queue(cl::sycl::default_selector{});
 
+    cl::sycl::buffer<float, 2> A(cl::sycl::range(32, 32));
+    cl::sycl::buffer<float, 2> B(cl::sycl::range(32, 32));
 
-return 0;
+    
+    auto matmul_task=[&](cl::sycl::handler& h) 
+    { 
+    
+    
+    
+    };
+    matmul_queue.submit(matmul_task);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
 
 }
