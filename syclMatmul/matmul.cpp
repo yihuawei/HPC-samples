813482ade2906bb4c65703ac39321ea1a678c10e
u65695 <u65695@login-2.aidevcloud>
2021-03-05 01:45:17 -0800

    update

--- syclMatmul/matmul.cpp
+++ syclMatmul/matmul.cpp
@@ -1,11 +1,29 @@
 #include <CL/sycl.hpp>
 #include "oneapi/mkl.hpp"
 
+const int row_A=32;
+const int col_A=128;
+const int row_B=128;
+const int col_B=64;
+
+
+
+
+
+
 int main(int argc,  char** argv){
+    
+    std::array<char,12> host_A;
+    std::array<char,12> host_B;
+    std::array<char,12> host_C;
+	
+	
+	
+	
     cl::sycl::queue matmul_queue(cl::sycl::default_selector{});
 
-    cl::sycl::buffer<float, 2> A(cl::sycl::range(32, 32));
-    cl::sycl::buffer<float, 2> B(cl::sycl::range(32, 32));
+    cl::sycl::buffer<float, 2> A(cl::sycl::range<2>(32, 32));
+    cl::sycl::buffer<float, 2> B(cl::sycl::range<2>(32, 32));
 
     
     auto matmul_task=[&](cl::sycl::handler& h) 
