5d3c63d26027613c1c0cecf135219c138752c566
u65695 <u65695@login-2.aidevcloud>
2021-03-05 04:23:29 -0800

    update

--- syclMatmul/matmul.cpp
+++ syclMatmul/matmul.cpp
@@ -28,14 +28,13 @@ int main(int argc,  char** argv){
     sycl::buffer<float, 2> buffer_A(host_A.data(), sycl::range(m, k));
     sycl::buffer<float, 2> buffer_B(host_B.data(), sycl::range(k, n));
     sycl::buffer<float, 2> buffer_C(host_C.data(), sycl::range(m, n));
-    
     auto matmul_task=[&](cl::sycl::handler& h) 
     { 
          sycl::accessor device_A = buffer_A.get_access<sycl::access::mode::read>(h);
          sycl::accessor device_B = buffer_B.get_access<sycl::access::mode::read>(h);
          sycl::accessor device_C = buffer_C.get_access<sycl::access::mode::discard_write>(h);
          
-	 int width_A = device_A.get_range()[1];;
+	 int width_A = buffer_A.get_range()[1];;
 	 h.parallel_for(sycl::range(m, n), [=](sycl::item<2> index) {
 	     int row = index[0];
              int col = index[1];
@@ -53,7 +52,32 @@ int main(int argc,  char** argv){
     sycl::queue matmul_queue(cl::sycl::default_selector{});
     matmul_queue.submit(matmul_task);
     
+
+    std::ostream_iterator<float> out_iter(std::cout, " ");
     
+    struct cout_class_func{
+        const char* str;
+	int index=0;
+	int width=0;
+        cout_class_func(const char* s, int width):str(s), width(width){
+	    std::cout<<std::string(s)<<end::endl;
+	}
+        void operator () (int i){
+	    if(index%width==0) std::cout<<endl;
+            std::cout<<i<<" ";
+            index++;
+        }
+	~cout_class_func(){
+	    std::cout<<std::string(s)<<end::endl;
+	}
+    };
+    
+    
+    for_each(host_A.cbegin(),host_A.cend(), cout_class_func("Matrix A", k));
+    for_each(host_B.cbegin(),host_B.cend(), cout_class_func("Matrix B", n));
+    for_each(host_C.cbegin(),host_C.cend(), cout_class_func("Matrix C", n))
+
+    return 0;
     double alpha = 1.0;
     double beta = 0.0;
     auto transA = oneapi::mkl::transpose::nontrans;
@@ -64,8 +88,8 @@ int main(int argc,  char** argv){
     auto gemm_buffer_A = buffer_A.reinterpret<float, 1>(sycl::range(m*k));
     auto gemm_buffer_B = buffer_B.reinterpret<float, 1>(sycl::range(k*n));
     auto gemm_buffer_C = buffer_C.reinterpret<float, 1>(sycl::range(m*n));
-    oneapi::mkl::blas::row_major::gemm(matmul_queue, transA, transB, m, n, k,
-                                           alpha, gemm_buffer_A, lda, gemm_buffer_B, ldb, beta, gemm_buffer_C, ldc);  
+    //oneapi::mkl::blas::row_major::gemm(matmul_queue, transA, transB, m, n, k,
+    //                                       alpha, gemm_buffer_A, lda, gemm_buffer_B, ldb, beta, gemm_buffer_C, ldc);  
     
     
     
